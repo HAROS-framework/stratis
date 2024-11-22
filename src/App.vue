8be44d588e2bc01908690a1ccc411b24dba8ca0e
André Santos <git-afsantos@users.noreply.github.com>
2024-11-22 16:42:53 +0000

    WIP layout

--- src/App.vue
+++ src/App.vue
@@ -7,83 +7,47 @@ const crumbStore = useBreadcrumbStore()
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <header class="app-header">
+    <HelloWorld msg="HAROSviz" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <p>crumbs: {{ crumbStore.crumbs }}</p>
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav class="app-nav">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">Issues</RouterLink>
+      <RouterLink to="/about">Source</RouterLink>
+      <RouterLink to="/about">Runtime</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
-header {
+<style>
+.app-header {
   line-height: 1.5;
-  max-height: 100vh;
+  display: flex;
+  flex-direction: column;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.app-nav {
+  font-size: 1.25rem;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
+.app-nav a.router-link-exact-active {
   color: var(--color-text);
+  font-weight: bold;
 }
 
-nav a.router-link-exact-active:hover {
+.app-nav a.router-link-exact-active:hover {
   background-color: transparent;
 }
 
-nav a {
+.app-nav a {
   display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
 }
 
-nav a:first-of-type {
+.app-nav a:first-of-type {
   border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
