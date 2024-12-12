42a6e6e089dda8796aa8326e93111941dae5acf3
André Santos <git-afsantos@users.noreply.github.com>
2024-12-12 15:32:03 +0000

    fix GH Pages

---
+++ docs/assets/SourceView-DrXWSVFa.js
@@ -0,0 +1,1 @@
+import{_ as s,o,c,a as r}from"./index-CA7bcTze.js";const t={},a={class:"source"};function n(_,e){return o(),c("div",a,e[0]||(e[0]=[r("h1",null,"This is the source page",-1)]))}const l=s(t,[["render",n]]);export{l as default};
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 // Copyright © 2024 André Santos
 
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
